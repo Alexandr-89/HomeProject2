b21cac91cf1ebeb2997a0ee6cc13e82478da2a36
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-10-29 19:58:54 +0300

    created animal objects in the main class

--- src/by/overone/homework1/Main.java
+++ src/by/overone/homework1/Main.java
@@ -5,6 +5,12 @@ public class Main {
 
         Zoo zoo = new Zoo();
 
-
+        Predators predator1 = new Predators(1, "Leva", 5, 80, "lione", "light brown");
+        Predators predator2 = new Predators(2, "Tigra", 10, 75, "tiger", "yeiiow-blak striped");
+        Herbivores herbivore1 = new Herbivores(3, "Rudy", 7, 320, "zebra", "ungulates");
+        Herbivores herbivore2 = new Herbivores(4, "Byrenka", 6, 450, "cow", "artiodactylc");
+        Rodents rodent1 = new Rodents(5, "Chip", 3, 0.25, "chipmunk", 4);
+        Rodents rodent2 = new Rodents(6, "Dail", 2, 0.2, "chipmunk", 4);
+        Rodents rodent3 = new Rodents(7,"Rocfor", 7, 0.2, "mouse", 0.2);
     }
 }
