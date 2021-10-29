b21f25d416cf24b502ae417fd7a61c993909c8a8
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-10-29 18:35:44 +0300

    first commit

---
+++ src/by/overone/homework1/Animal.java
@@ -0,0 +1,11 @@
+package by.overone.homework1;
+
+public class Animal {
+
+    private int id;
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+
+}
