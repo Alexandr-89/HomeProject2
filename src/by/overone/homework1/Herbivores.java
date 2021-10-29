a27c3988625f05262a7c6054bc25fa139993338b
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-10-29 19:26:57 +0300

    added a constructor and a method to eat for herbivores

--- src/by/overone/homework1/Herbivores.java
+++ src/by/overone/homework1/Herbivores.java
@@ -1,6 +1,22 @@
 package by.overone.homework1;
 
-public class Herbivores {
+public class Herbivores extends Animal {
 
     private String view;
+
+    public Herbivores(int id, String name, int age, double weight, String type, String view) {
+        super(id, name, age, weight, type);
+        this.view=view;
+    }
+
+    @Override
+    public void eat(String food) {
+        String bestFood = "grass";
+        if (bestFood==food){
+            System.out.println("This is the best food, I am very happy");
+        }else {
+            System.out.println("This is bad food, I'll trample you");
+        }
+
+    }
 }
