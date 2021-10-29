7b118816809401d8d6e84c3460a3bef2ebcb7876
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-10-29 19:36:55 +0300

    added a constructor and a method to eat for rodents

--- src/by/overone/homework1/Rodents.java
+++ src/by/overone/homework1/Rodents.java
@@ -3,4 +3,21 @@ package by.overone.homework1;
 public class Rodents extends Animal{
 
     private double lengthOfIncisors;
+
+    public Rodents(int id, String name, int age, double weight, String type, double lengthOfIncisors) {
+        super(id, name, age, weight, type);
+        this.lengthOfIncisors=lengthOfIncisors;
+    }
+
+    @Override
+    public void eat(String food) {
+        String bestFood = "nuts";
+        if (bestFood==food){
+            System.out.println("This is the best food, I am very happy");
+        }else {
+            System.out.println("This is very bad food, I'll chew through your shoes");
+        }
+
+    }
+
 }
