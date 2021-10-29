7b423e6357a6f6da582172cb8a0c44bd5d648d4f
Alexandr Kulakevich <alexandr.kulakevich1989@gmail.ru>
2021-10-29 19:32:20 +0300

    created the predators class

--- src/by/overone/homework1/Predators.java
+++ src/by/overone/homework1/Predators.java
@@ -4,5 +4,22 @@ public class Predators extends Animal{
 
     private String colour;
 
-    
+    public Predators(int id, String name, int age, double weight, String type, String colour) {
+        super(id, name, age, weight, type);
+        this.colour=colour;
+    }
+
+
+
+
+    @Override
+    public void eat(String food) {
+        String bestFood = "meat";
+        if (bestFood==food){
+            System.out.println("This is the best food, I am very happy");
+        }else {
+            System.out.println("This is bad food, I'll eat you");
+        }
+    }
+
 }
